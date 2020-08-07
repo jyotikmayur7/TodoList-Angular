5579244da0ff713cb56c063085057d6663f41cd6
orangekun9 <heyjyotik@gmail.com>
2020-08-07 12:24:05 +0100

    Added HttpClientModule in app.module and used it on todo.service and initialized it on constructor for later api calls

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   getTodos() {
     return [
