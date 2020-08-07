b09e1904c9f3e4af8c287e52981cd59585b65e7a
orangekun9 <heyjyotik@gmail.com>
2020-08-07 11:28:21 +0100

    Added Todo model, todo-item-component and also added css in global style

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
+  todos: Todo[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Task One',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Task Two',
+        completed: true,
+      },
+      {
+        id: 3,
+        title: 'Task Three',
+        completed: false,
+      },
+    ];
   }
-
 }
