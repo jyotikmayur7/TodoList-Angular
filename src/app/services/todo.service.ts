c75fc3ce99248289d7302cbbaf9b5e92d1f118b7
orangekun9 <heyjyotik@gmail.com>
2020-08-07 12:41:47 +0100

    Made Api request to JsonPlaceHolder to get free todo data and handled todo's data asynchronously in todo.component

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
+  todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit: string = '?_limit=7';
+
   constructor(private http: HttpClient) {}
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'Task One',
-        completed: false,
-      },
-      {
-        id: 2,
-        title: 'Task Two',
-        completed: true,
-      },
-      {
-        id: 3,
-        title: 'Task Three',
-        completed: false,
-      },
-    ];
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 }
